4bd9aee17bd38b00edea676bf238bdc87076d95a
Nikolay <nyk190@mail.ru>
2022-03-31 23:31:58 +0300

    Add gridChange and pixelEvent functions
    gridChange function change number of divs in grid, when user change
    input range value
    
    pixelEvent function change background color on black, when cursor enter
    the pixel-div

---
+++ script.js
@@ -0,0 +1,37 @@
+let switchGridSize = document.querySelector('.grid-size__input');
+let gridDraw = document.querySelector('.container__grid');
+let container = document.querySelector('.container');
+switchGridSize.addEventListener('input', gridChange);
+gridChange();
+
+function gridChange (){
+    let gridDraw = document.querySelector('.container__grid');
+    container.removeChild(gridDraw);
+    let newGrid = document.createElement('div')
+    newGrid.classList.add('container__grid');
+    let n = switchGridSize.value;
+    let pixelSize = 100/n + '%';
+    let pixel = document.createElement('div');
+    pixel.style.cssText = `width: ${pixelSize}; height: ${pixelSize};`;
+    pixel.classList.add('grid__pixel');
+    for(let i = 0; i < n*n; i++) {
+        let pixelSize = 100/n + '%';
+        let pixel = document.createElement('div');
+        pixel.style.cssText = `width: ${pixelSize}; height: ${pixelSize};`;
+        pixel.classList.add('grid__pixel');
+        newGrid.appendChild(pixel);
+    }
+
+    container.appendChild(newGrid);
+
+    let pixels = document.querySelectorAll('.grid__pixel');
+    pixelEvent(pixels);
+}
+
+function pixelEvent(pixels) {
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mouseenter', (evt)=>{
+            evt.target.style['background-color'] = '#000';
+        });
+    });
+}
